f8b807a71a33112ea8eabbeb066015d05b036e2a
littledumbco <littledumbco@gmail.com>
2017-10-15 12:25:06 +0100

    UI Elements & Dialog Box
    Added in a few simple UI elements: image and text box. Transformed the
    text box into a dialog box, where the user can tap space to change the
    text to the next paragraph.

---
+++ Assets/DialogController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI; // adding in the Unity UI library
+using System.Collections;
+
+public class DialogController : MonoBehaviour {
+	
+	public Text DialogBox;
+	
+	// Use this for initialization
+	void Start () {	
+		// assigning the text to the dialog box
+		DialogBox.text = "Hello World";
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// if the user taps space, change the dialog text
+		if (Input.GetKeyDown (KeyCode.Space)){
+			DialogBox.text = "You are a man or at least, that's what they've" + 
+							" told you. You find yourself in a dark and dank" + 
+							" prison cell. It is grim. Around you are just a" + 
+							" few minor things: Some dirty sheets, a cracked" +
+							" mirror and a solid steel, locked prison door.\n\n" +
+							"Tap S to view the sheets, Tap M to view the mirror," +
+							" Tap D to view the door.";
+		}
+	
+	}
+}
